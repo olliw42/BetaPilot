bf42de9f130300c594bc54a57d11891989e009fc
olliw42 <waldmanns@gmx.de>
2021-07-28 17:28:24 +0200

    add all files and changes by hand., compiles fine

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -7,6 +7,11 @@
 #include "ap_version.h"
 
 #define THISFIRMWARE "ArduPlane V4.1.0beta4"
+//OW
+#undef THISFIRMWARE
+#include "../libraries/bp_version.h"
+#define THISFIRMWARE "ArduPlane V4.1.0beta4 " BETAPILOTVERSION " 20210520"
+//OWEND
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,0,FIRMWARE_VERSION_TYPE_BETA+3
