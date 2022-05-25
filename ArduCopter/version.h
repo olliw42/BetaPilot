7a6613e68ad9bbff6986cd6f2f6ab74765a80350
olliw42 <waldmanns@gmx.de>
2022-05-25 20:49:39 +0200

    v053.1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -7,6 +7,11 @@
 #include "ap_version.h"
 
 #define THISFIRMWARE "ArduCopter V4.2.0"
+//OW
+#undef THISFIRMWARE
+#include "../libraries/bp_version.h"
+#define THISFIRMWARE "BetaCopter V4.2.0 " BETAPILOTVERSION " 20220525"
+//OWEND
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,0,FIRMWARE_VERSION_TYPE_OFFICIAL
