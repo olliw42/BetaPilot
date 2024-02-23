1468035c6a73d495cacfb14440353b2e5afb6ccf
olliw42 <waldmanns@gmx.de>
2024-02-23 09:52:53 +0100

    cc vbump

--- libraries/bp_version.h
+++ libraries/bp_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define BETAPILOTVERSION "v060e"
-#define DATEOFBASEBRANCH "20240221"
+#define DATEOFBASEBRANCH "20240223"
 
 /*
 search for //OW to find all changes  THR_MINSPD THR_SLEW SUPP_MAN waiting for rudder release
